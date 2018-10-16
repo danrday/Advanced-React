553a574a1311ad41305bf87dfab48882d33aa7c4
euf7315 <Daniel.Day@ParallonTechnology.com>
2018-10-16 15:37:42 -0500

    get backend server up

---
+++ sick-fits/backend/src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config({path: '.env'})
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+/*
+to do:
+    use express middleware to handle cookies
+    use express middleware to populate current user*/
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, deets => {
+    console.log(`server is now running on port: http:/localhost:${deets.port}`)
+})
